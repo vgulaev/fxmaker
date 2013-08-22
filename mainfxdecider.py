be55f07f97b0ad0145989e7b601adbaf6b84e69c
vgulaev <vgulaev@yandex.ru>
2013-08-22 17:20:46 +0600

    Up

--- mainfxdecider.py
+++ mainfxdecider.py
@@ -1,1 +1,15 @@
-print("dfdfd")
+import json
+mydict = {"AccountBalance" : 2,
+          "Orders": [{"ticket" : 1}, {"ticket" : 2}]}
+handle = open("my.json", "w")
+handle.write(str(mydict))
+handle.close()
+
+#json_file = open("my.json","r")
+#json_string = json_file.read()
+#json_file.close() 
+
+#seo_tags = eval(json_string)
+
+#print(seo_tags)
+#print(seo_tags["abc"])
